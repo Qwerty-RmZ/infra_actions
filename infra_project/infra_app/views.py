1a2a2bda672995354f55ce530fce8fe6ae124190
Qwerty-RmZ <85193325+Qwerty-RmZ@users.noreply.github.com>
2022-03-01 19:45:58 +0300

    Update views.py

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!1111111111')
+    return HttpResponse('У меня получилось!11111111112')
 
 
 def second_page(request):
